26758f4b668034c8ecfa67a3394508053c1153d1
I am <88476416+FirstSolar@users.noreply.github.com>
2024-06-27 10:22:10 +0300

    Create footer.js
    Create footer

---
+++ scripts/footer.js
@@ -0,0 +1,14 @@
+// Reusable Elements
+
+// Footer
+footerContainer = document.querySelector("#footerContainer");
+
+function footerContainerF(footerContainer) {
+  if (footerContainer != null) {
+    // Define footer content:
+    footerContainer.innerHTML += `
+Copyright © 2024 Vladislav Kazantsev. <span class="nowrap">All Rights Reserved.</span>
+      `
+  }
+}
+footerContainerF(footerContainer);
