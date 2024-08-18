66b096ad5d7ba0a4d1d0e4b537be14cb986db59c
I am <88476416+FirstSolar@users.noreply.github.com>
2024-08-18 11:50:47 +0300

    Update elements.js
    * Create custom reusable scroll to top button that is automatically generated on webpages.
    * Create source code comments.

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,3 +1,41 @@
+// The Library of Custom Reusable Web Elements
+// This file is the library containing my collection of custom, reusable web elements
+// that can be used across different parts of the project. These elements go beyond what browsers provide,
+// allowing for expanded capabilities and functionality in the project.
+
+// Scroll to Top Button
+class scrollToTop extends HTMLElement {
+  connectedCallback() {
+    if (window.innerWidth <= 600) {
+      this.innerHTML += `
+      <a href="#" class="to-top">
+        <span class="badge arrow-up">⬆</span>
+      </a>
+      `
+    } else {
+      this.innerHTML += `
+      <a href="#" class="to-top">
+        <img src="./assets/arrow.png" alt="Arrow Badge" class="badge arrow-up"/>
+      </a>
+      `
+    }
+  }
+}
+customElements.define("scroll-to-top-button", scrollToTop);
+console.log(window.innerWidth);
+
+const scrollToTopButton = document.createElement("scroll-to-top-button");
+document.querySelector("#about").appendChild(scrollToTopButton);
+
+const toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 450) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
+
 // Footer
 footerContainer = document.querySelector("#footerContainer");
 
