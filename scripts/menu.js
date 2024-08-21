fa9ae2235283d57404e6930f27d2ef9b83abfcdb
I am <88476416+FirstSolar@users.noreply.github.com>
2024-08-21 10:55:56 +0300

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector(".menu-links");
-const badge = document.querySelector(".hamburger-icon");
+const badge = document.querySelector(".hamburger-badge");
 
 function toggleMenu() {
   menu.classList.toggle("open");
@@ -7,7 +7,7 @@ function toggleMenu() {
 }
 
 window.onclick = function(event) {
-  if (!event.target.matches(".menu-links") && !event.target.matches(".hamburger-icon") && !event.target.matches(".hamburger-icon span") && menu.classList.contains("open") === true) {
+  if (!event.target.matches(".menu-links") && !event.target.matches(".hamburger-badge") && !event.target.matches(".hamburger-badge span") && menu.classList.contains("open") === true) {
     menu.classList.toggle("open");
     badge.classList.toggle("open");
   }
