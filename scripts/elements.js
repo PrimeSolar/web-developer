82791a9fc8405322a966d48f62ae721c086e440b
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-04-29 10:57:21 +0300

    Update elements.js
    Update the library of custom reusable web elements for Web Developer.

--- scripts/elements.js
+++ scripts/elements.js
@@ -3,7 +3,7 @@
 // that can be used across different parts of the project. These elements go beyond what browsers provide,
 // allowing for expanded capabilities and functionality in the project.
 
-// Scroll to Top Button
+// The Scroll to Top Button
 class ToTop extends HTMLElement {
   connectedCallback() {
     if (window.innerWidth <= 600) {
@@ -11,36 +11,43 @@ class ToTop extends HTMLElement {
       <a href="#">
         <span class="badge arrow-up">⬆</span>
       </a>
-      `
+      `;
     } else {
       this.innerHTML += `
       <a href="#">
         <img src="./assets/arrow-up.png" alt="Scroll to top" title="Scroll to top" aria-label="Scroll to top" class="badge arrow-up" />
       </a>
-      `
+      `;
     }
   }
 }
 customElements.define("to-top", ToTop);
 
-const toTop = document.querySelector("#about").appendChild(document.createElement("to-top")).querySelector("a");
+// The Years of Experience
+let experiencesText = document.querySelector("#about .card-text");
+experiencesText.innerHTML =
+  new Date().getFullYear() - 2019 + experiencesText.innerHTML;
+
+const toTop = document
+  .querySelector("#about")
+  .appendChild(document.createElement("to-top"))
+  .querySelector("a");
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 450) {
     toTop.classList.add("active");
   } else {
     toTop.classList.remove("active");
   }
-})
+});
 
-// Footer
-footerContainer = document.querySelector("#footerContainer");
+// The Footer
+const footerContainer = document.querySelector("#footerContainer");
+const year = new Date().getFullYear();
 
 function footerContainerF(footerContainer) {
   if (footerContainer != null) {
-    // Define footer content:
-    footerContainer.innerHTML += `
-Copyright © 2024 Vladislav Kazantsev. <span class="nowrap">All Rights Reserved.</span>
-      `
+    // Define the footer content:
+    footerContainer.innerHTML += `Copyright © ${year} Vladislav Kazantsev. <span class="nowrap">All Rights Reserved.</span>`;
   }
 }
 footerContainerF(footerContainer);
