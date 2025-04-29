93de8173d6124ffa8d909e6d0f105ca6cc2a1d4f
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-04-29 10:58:36 +0300

    Update menu.js
    Update the menu script for Web Developer.

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,14 +1,22 @@
 const menu = document.querySelector(".menu-links");
 const badge = document.querySelector(".hamburger-badge");
 
+menu.addEventListener("click", toggleMenu);
+badge.addEventListener("click", toggleMenu);
+
 function toggleMenu() {
   menu.classList.toggle("open");
   badge.classList.toggle("open");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches(".menu-links") && !event.target.matches(".hamburger-badge") && !event.target.matches(".hamburger-badge span") && menu.classList.contains("open") === true) {
+window.onclick = function (event) {
+  if (
+    !event.target.matches(".menu-links") &&
+    !event.target.matches(".hamburger-badge") &&
+    !event.target.matches(".hamburger-badge span") &&
+    menu.classList.contains("open") === true
+  ) {
     menu.classList.toggle("open");
     badge.classList.toggle("open");
   }
-}
+};
