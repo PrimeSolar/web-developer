b7f76346a413c45590e6c86407d504390f46fa04
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-08-24 13:55:53 +0300

    Create scroll-animation.js
    Create an original version of a script for a scroll animation.

---
+++ scripts/scroll-animation.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll(".scroll-animation");
+
+const options = {
+  root: null,
+  threshold: 0.1,
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+      observer.unobserve(entry.target);
+    }
+  });
+}, options);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
