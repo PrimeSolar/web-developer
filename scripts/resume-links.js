5cfbc396c49bdaab46e17c25f3a752916aa6794e
I am <88476416+PrimeSolar@users.noreply.github.com>
2026-02-18 04:44:26 +0300

    Update the script to fetch project data
    Fetch project data from the main page and append it to the "Projects" section of the resume.

--- scripts/resume-links.js
+++ scripts/resume-links.js
@@ -13,6 +13,34 @@
  * For inquiries about collaboration, usage outside exploratory purposes, or permissions, please contact: hypervisor7@pm.me
  */
 
+const projectCollection = document.querySelector(".project-collection");
+
+fetch("/index.html")
+  .then((response) => response.text())
+  .then((html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    const projectCards = Array.from(doc.querySelectorAll(".project-card"));
+    projectCards.forEach((project) => {
+      const name = project
+        .querySelector(".project-title")
+        .innerHTML.replace(/<br\s*\/?>/gi, " ")
+        .trim();
+      const githubHref = project.querySelector(
+        ".button-container > a:nth-of-type(1)"
+      ).href;
+      const projectHref = project.querySelector(
+        ".button-container > a:nth-of-type(2)"
+      ).href;
+      const li = document.createElement("li");
+      li.innerHTML = `${name}:
+      <li><a href=${githubHref} rel="noopener noreferrer">GitHub</a></li>
+      <li><a href=${projectHref} rel="noopener noreferrer">Live Demo</a></li>
+      `;
+      projectCollection.appendChild(li);
+    });
+  })
+  .catch(console.error);
+
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
